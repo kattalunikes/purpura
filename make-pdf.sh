5a0410d194725217d7e7e293e3eb8f5097de7cb8
Kattalunikes <kattalunikes+git@gmail.com>
2026-05-13 18:47:11 +0000

    Season 4: 1--10

--- make-pdf.sh
+++ make-pdf.sh
@@ -4,6 +4,7 @@ pandoc -o moby-purpura.$TYPE --toc --toc-depth=2 \
   Season\ 01/*md \
   Season\ 02/*md \
   Season\ 03/*md \
+  Season\ 04/*md \
   --metadata title="Purpura!" \
   --metadata subtitle="Die Legenden der Moby Purpura" \
   --metadata author="von Fear und der Crew aus Tentakelhausen" \
