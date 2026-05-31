e6beee906cd4ac29ee66df1bf07f83f4a2cf5138
Kattalunikes <kattalunikes+git@gmail.com>
2026-05-31 11:52:45 +0000

    Adds Status to Github Page

--- make-pages.sh
+++ make-pages.sh
@@ -4,9 +4,22 @@ mkdir -p $OUTDIR
 cp purpura.* $OUTDIR/
 
 cat <<EOD | pandoc -o $OUTDIR/index.html
-# Purpura
+# Purpura!
+
+Purpura! - Die Legenden der Moby Purpura
+
+von Fear und der Crew aus Tentakelhausen (Albi, Amii, Devil, Luna, Luxi, Mitch, Purpur, Shanty, Vio)
+
+## Downloads
 
 * [View HTML](purpura.html)
 * [Download PDF](purpura.pdf)
 * [Download ePub](purpura.epub)
+
+## Status
+
+Most Recent Updates
+\`\`\`
+$(git log -n 3 | sed 's/[a-z+]\+@[a-z.]\+/.../')
+\`\`\`
 EOD
